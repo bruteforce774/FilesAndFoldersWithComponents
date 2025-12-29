1dbb10809beac9baa236bbc0d1add9df044e6f06
Daniel Andreas Wang <danielaw@getacademy.no>
2025-12-29 20:41:31 +0100

    use emit

--- FilesAndFoldersVue/src/components/FilesAndFolders.vue
+++ FilesAndFoldersVue/src/components/FilesAndFolders.vue
@@ -6,21 +6,40 @@ const props = defineProps<{
   parentFolder?: number | false,
   markedFilesAndFolders?: number[],
 }>();
+
+// emit for original custom event "selected"
+const emit = defineEmits<{
+  selected: [id: number]
+}>();
+
+function handleSelect(id: number) {
+  // corresponds to idStr in original
+  emit('selected', id);
+}
 </script>
 
 <template>
   <fieldset>
     <legend>Mapper og filer</legend>
-    <li v-for="item in props.items" :key="item.id">
-      <span v-if="item.content">🗎</span>
-      <span v-else>📁</span>
-      {{ item.name }}
-    </li>
+    <ul>
+      <li v-for="item in props.items" :key="item.id">
+        <!-- link with click handler -->
+        <a href="#" @click.prevent="handleSelect(item.id)">
+        <span v-if="item.content">🗎</span>
+        <span v-else>📁</span>
+        {{ item.name }}
+        </a>
+      </li>
+    </ul>
   </fieldset>
 </template>
 
 <style scoped>
+  a {
+    text-decoration: none;
+  }
   li {
     list-style: none;
+    padding: 0;
   }
 </style>
